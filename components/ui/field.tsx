c3398e560f667a65a7e1b1556104bf237e44516d
Abolfazl <a.abbasianco@gmail.com>
2026-07-19 19:12:37 +0330

    Added separator component

--- components/ui/field.tsx
+++ components/ui/field.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/primitive/separator"
 
 function FieldSet({ className, ...props }: React.ComponentProps<"fieldset">) {
   return (
